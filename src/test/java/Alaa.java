e68db6f9d4eb78a644b069fa53e18f55674b9a7d
Mahmudov, Khazar <C-Khazar.Mahmudov1@charter.com>
2021-10-20 19:01:25 -0600

    changed Alaa

--- src/test/java/Alaa.java
+++ src/test/java/Alaa.java
@@ -2,6 +2,6 @@ public class Alaa {
     public static void main(String[] args) {
         System.out.println("I love you, Alaa");
 
-        String name = "Alaa";
+        String name = "Alaa90";
     }
 }
